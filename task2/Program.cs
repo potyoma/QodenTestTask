33d890e4812575c0800a4b3c51be5ce2066a4761
Artyom Novoselov <novoselov_artyom@tutanota.com>
2020-10-19 19:46:43 +0300

    task2's solved!

--- task2/Program.cs
+++ task2/Program.cs
@@ -18,15 +18,20 @@ namespace task2
                 .Select(c => new
                 {
                     Word = c,
-                    Count = (int)Math.Ceiling((double)text.Count(e => e == c) / text.Length * 10)
+                    Count = (double)text.Count(e => e == c) / text.Length
                 })
                 .OrderBy(c => c.Count);
 
+            var mostCommon = query
+                .Max(e => e.Count);
+
             foreach (var pair in query)
             {
+                var frequency = (int)Math.Round(pair.Count * 10 / mostCommon, 0);
+
                 char[] underlines = new char[maxLength - pair.Word.Length];
                 Array.Fill(underlines, '_');
-                char[] dots = new char[pair.Count];
+                char[] dots = new char[frequency];
                 Array.Fill(dots, '.');
                 WriteLine($"{new string(underlines)}{pair.Word} {new string(dots)}");
             }
